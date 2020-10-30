aea270ac22e2ed35621d29f20d6fd2c4933a4940
suvinsnair <suvin20156@gmail.com>
2020-10-31 00:25:57 +0530

    added select2 configuration

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     // .splitEntryChunks()
-
+// 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     // .enableSingleRuntimeChunk()
